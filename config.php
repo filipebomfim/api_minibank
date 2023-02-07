3bf46351a9a8aaa047314e841c6599567d29e8e5
Filipe Furtado <lipe.bomfim99@gmail.com>
2023-02-06 23:01:49 -0300

    Criação da base de dados, do DER e do arquivo PHP de conexão com o banco

--- config.php
+++ config.php
@@ -2,6 +2,12 @@
     //Set do zona para data e hora do servidor
     date_default_timezone_set('America/Sao_Paulo');
 
+    //Dados de acesso ao banco de dados
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASSWORD','');
+    define('DATABASE','minibank');
+
     //Função para retorno de resposta em JSON
     function _response_( $success = true, $title = null, $data = array(), $response_cod = 200 )
     {
---
+++ minibank.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 07-Fev-2023 às 02:56
+-- Versão do servidor: 10.4.16-MariaDB
+-- versão do PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE minibank;
+USE minibank;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `cod` int(6) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `branch` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `type` enum('deposit','withdrawal','transfer') NOT NULL,
+  `value` float NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cod` (`cod`),
+  ADD KEY `fk_account_client` (`client_id`);
+
+--
+-- Índices para tabela `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_transaction_account` (`account_id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `fk_account_client` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`);
+
+--
+-- Limitadores para a tabela `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `fk_transaction_account` FOREIGN KEY (`account_id`) REFERENCES `transaction` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
